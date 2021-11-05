52b52b18dd8515187660537200b27843cf64c738
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-11-05 12:16:33 +0700

    useRef

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,47 +1,42 @@
-import React, { useEffect, useLayoutEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../App.css";
-// 1. useEffect (callback)
-//  - Callback luôn gọi khi component re-render
-//  - Gọi callback sau khi component thêm element vào DOM
-// 2. useEffect (callback,[])
-//  - Chỉ gọi callback 1 lần mỗi khi component được mounted
-// 3. useEffect (callback,[deps])
-//  - Callback sẽ được gọi lại mỗi khi deps thay đổi
-//================
 
-// 1. Callback luôn gọi khi component được mounted
-
-///////////////////////
-
-// useEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật lại DOM (mutated)
-// 3. Render lại UI
-// 4. Gọi cleanup nep deps thay đổi
-// 5. Gọi useEffect callback
-
-// useLayoutEffect
-// 1. Cập nhật lại state
-// 2. Cập nhật lại DOM (mutated)
-// 3. Gọi cleanup nếu deps thay đổi (sync)
-// 4. Gọi useLayoutEffect callback (sync)
-// 5. Render lai UI
-
+//useRef
+// Lưu một giá trị qua một tham chiếu bên ngoài commponent
 function Content() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(60);
 
-  useLayoutEffect(() => {
-    if (count > 3) setCount(0);
-  }, [count]);
+  const timerId = useRef()
+  const prevCount = useRef()
+  const h1Ref = useRef()
 
-  const handle = () => {
-    setCount(count + 1);
+  useEffect(()=>{
+    prevCount.current = count
+  },[count])
+
+  useEffect(()=>{
+    const rect = h1Ref.current.getBoundingClientRect()
+    console.log(rect)
+  })
+
+  const handleStart = () => {
+    timerId.current = setInterval(()=>{
+      setCount(prevCount=>prevCount-1)
+    },1000)
+  };
+
+  const handleStop = () => {
+    clearInterval(timerId.current);
   };
 
   return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={handle}>Click me!</button>
+    <div style={{ padding: 20 }}>
+      <h1 ref={h1Ref}>{count}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <h2>
+        {count} : {prevCount.current}
+      </h2>
     </div>
   );
 }
