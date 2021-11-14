fad652fd4ca0c0d1a31bad9b617fb57e270717a2
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-11-15 01:13:09 +0700

    useReducer todo

--- src/App.js
+++ src/App.js
@@ -1,44 +1,98 @@
-import React, { useReducer } from "react";
+import React, { useReducer, useRef } from "react";
 import "./App.css";
-// useState
-// 1. Init state: 0.
-// 2. Actions: Up (state + 1) / Down (state - 1)
 
 // useReducer
-// 1. Init state: 0.
-// 2. Actions: Up (state + 1) / Down (state - 1)
-// 3. Reducer
-// 4. Dispatch
-
-//Init state
-const initState = 0;
-
-// Actions
-const UP_ACTION = "up";
-const DOWN_ACTION = "down";
-
-// Reducer
-const reducer = (state, action) => {
-  switch (action) {
-    case UP_ACTION:
-      return state + 1;
-    case DOWN_ACTION:
-      return state - 1;
-    default:
-      throw new Error("Invalid action");
-  }
+// 1. Init state
+const initState = {
+  job: "",
+  jobs: [],
 };
 
+// 2. Actions
+const SET_JOB = "set_job";
+const ADD_JOB = "add_job";
+const DELETE_JOB = "delete_job";
 
+const setJob = (payload) => {
+  return {
+    type: SET_JOB,
+    payload,
+  };
+};
+
+const addJob = (payload) => {
+  return {
+    type: ADD_JOB,
+    payload,
+  };
+};
+
+const deleteJob = (payload) => {
+  return {
+    type: DELETE_JOB,
+    payload,
+  };
+};
+
+// 3. Reducer
+const reducer = (state, action) => {
+  switch (action.type) {
+    case SET_JOB:
+      return {
+        ...state,
+        job: action.payload,
+      };
+    case ADD_JOB:
+      return {
+        ...state,
+        jobs: [...state.jobs, action.payload], //trong es6 đối số đầu là bảo lưu cái cũ,
+        // cái sau là thêm vào phần tử mới
+      };
+    case DELETE_JOB:
+      const newJobs = [...state.jobs];
+      newJobs.splice(action.payload, 1);
+      return {
+        ...state,
+        jobs: newJobs, //trong es6 đối số đầu là bảo lưu cái cũ,
+        // cái sau là thêm vào phần tử mới
+      };
+    default:
+      throw new Error("Invalid action.");
+  }
+};
+// 4. Dispatch
 
 function App() {
-  const [count, disPatch] = useReducer(reducer, initState);
+  const [state, dispath] = useReducer(reducer, initState);
+  const { job, jobs } = state;
+  const inputRef = useRef();
+
+  const handleSubmit = () => {
+    dispath(addJob(job));
+    dispath(setJob(""));
+    inputRef.current.focus();
+  };
 
   return (
     <React.Fragment>
-      <h1>{count}</h1>
-      <button onClick={() => disPatch(DOWN_ACTION)}>Down</button>
-      <button onClick={() => disPatch(UP_ACTION)}>Up</button>
+      <h3>Todo</h3>
+      <input
+        value={job}
+        ref={inputRef}
+        onChange={(e) => {
+          dispath(setJob(e.target.value));
+        }}
+        placeholder="Enter todo..."
+      />
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job, index) => (
+          <li key={index}>
+            {job}
+            <span onClick={() => dispath(deleteJob(index))}> &times;</span>
+          </li>
+        ))}
+      </ul>
     </React.Fragment>
   );
 }
