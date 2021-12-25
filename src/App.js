1110468709e4f159ea2221f47f723801851da26e
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-12-25 09:46:34 +0700

    start

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { useRef } from "react";
-import Button from "./components/Button/Button";
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import ContactPage from "./components/Pages/Contact";
+import HomePage from "./components/Pages/Home";
+import NewsPage from "./components/Pages/News";
 
 function App() {
-  
   return (
     <>
-      <Button/>
-      <Button primary/>
-      <Button primary disabled/>
+      <div className="app">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/news">News</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/news" element={<NewsPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
