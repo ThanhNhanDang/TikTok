15411758bcbd41452e1cadcd1df7ed669d3d6386
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2021-12-25 10:03:30 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import ContactPage from "./components/Pages/Contact";
-import HomePage from "./components/Pages/Home";
-import NewsPage from "./components/Pages/News";
 
 function App() {
   return (
     <>
-      <div className="app">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/news">News</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/news" element={<NewsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </div>
+
     </>
   );
 }
