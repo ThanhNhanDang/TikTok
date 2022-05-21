cc357bbedbc6e28a9ed80551c829feea84910943
ThanhNhanDang <64689691+ThanhNhanDang@users.noreply.github.com>
2022-05-21 22:11:19 +0700

    Cấu hình Router và xây dựng cơ chế tải Layout

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './components/GlobalStyles';
 import App from '~/App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
-            <GlobalStyles>
-                <App />
-            </GlobalStyles>
-        </Router>
+        <GlobalStyles>
+            <App />
+        </GlobalStyles>
     </React.StrictMode>,
     document.getElementById('root'),
 );
